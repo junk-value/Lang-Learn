f1db0ae5243b19282594102e1aa03a55eec227c4
junk-value <tejalsharmak@gmail.com>
2025-08-02 13:20:24 +0530

    Button integrated

--- frontend/learn-lang/src/App.jsx
+++ frontend/learn-lang/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Header from './components/Header.jsx'
 import Footer from './components/Footer.jsx'
+import Button from './components/Button.jsx'
 import './App.css'
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <>
        <Header />
       <h1>Welcome to LearnLang!</h1>
+      <Button text="Learn Now" onClick={() => alert('Clicked')} />
       <Footer/>
     </>
   )
