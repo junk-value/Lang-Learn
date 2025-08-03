856aeac3d9f078d1a766e1db3a1ebd91d829f3ca
junk-value <tejalsharmak@gmail.com>
2025-08-03 23:20:37 +0530

    Card Integrated

--- frontend/learn-lang/src/App.jsx
+++ frontend/learn-lang/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import Header from './components/Header.jsx'
 import Footer from './components/Footer.jsx'
 import Button from './components/Button.jsx'
+import Card from './components/Card.jsx';
 import './App.css'
 
 function App() {
@@ -9,8 +10,9 @@ function App() {
 
   return (
     <>
-       <Header />
+      <Header />
       <h1>Welcome to LearnLang!</h1>
+      <Card title="Welcome" content="This is a learning App."/>
       <Button text="Learn Now" onClick={() => alert('Clicked')} />
       <Footer/>
     </>
