18e2c7191ea226bac69eb451a8def47205c306cc
junk-value <tejalsharmak@gmail.com>
2025-08-05 18:00:31 +0530

    Rendering Lesson List on main page

--- frontend/learn-lang/src/App.jsx
+++ frontend/learn-lang/src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/Header.jsx'
 import Footer from './components/Footer.jsx'
 import Button from './components/Button.jsx'
 import Card from './components/Card.jsx';
+import LessonList from './components/LessonList';
 import './App.css'
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Header />
       <h1>Welcome to LearnLang!</h1>
       <Card title="Welcome" content="This is a learning App."/>
+      <LessonList />
       <Button text="Learn Now" onClick={() => alert('Clicked')} />
       <Footer/>
     </>
