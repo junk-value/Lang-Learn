581d5fa22d2e5841d977821d7bb54f91d8941ff8
junk-value <tejalsharmak@gmail.com>
2025-08-05 17:57:06 +0530

    Dummy lessons

---
+++ frontend/learn-lang/src/data/lessons.js
@@ -0,0 +1,7 @@
+const lessons = [
+  { id: 1, title: "Basics of Spanish", level: "Beginner" },
+  { id: 2, title: "Intermediate French", level: "Intermediate" },
+  { id: 3, title: "Advanced German Grammar", level: "Advanced" },
+];
+
+export default lessons;
