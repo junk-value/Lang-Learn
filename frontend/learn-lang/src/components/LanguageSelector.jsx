70b6b7b2734b89670769be52cf76a406aea158d3
junk-value <tejalsharmak@gmail.com>
2025-08-06 12:22:21 +0530

    Language Selector

---
+++ frontend/learn-lang/src/components/LanguageSelector.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const LanguageSelector = () => {
+  return (
+    <div>
+      <label>Select Language: </label>
+      <select>
+        {/* Options to be added */}
+      </select>
+    </div>
+  );
+};
+
+export default LanguageSelector;
