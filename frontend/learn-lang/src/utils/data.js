18712cd825149b3842cdee642c1c920f3b930d40
junk-value <tejalsharmak@gmail.com>
2025-08-11 23:52:00 +0530

    mock words added

---
+++ frontend/learn-lang/src/utils/data.js
@@ -0,0 +1,7 @@
+export const wordsList = [
+  { en: "Hello", es: "Hola", fr: "Bonjour" },
+  { en: "Thank you", es: "Gracias", fr: "Merci" },
+  { en: "Goodbye", es: "Adiós", fr: "Au revoir" },
+  { en: "Yes", es: "Sí", fr: "Oui" },
+  { en: "No", es: "No", fr: "Non" }
+];
